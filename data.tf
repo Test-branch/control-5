ae3097df79bc205203c8fd3ae893d98278884108
Aravinda Babu <babu_chindukuri@optum.com>
2025-01-28 13:22:30 +0530

    j

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-data "github_repositories" "repos" {
-  query = "org:${var.github_organization}"
-}
+# data "github_repositories" "repos" {
+#   query = "org:${var.github_organization}"
+# }
 
 data "github_repository" "default_branch" {
   for_each = toset(data.github_repositories.repos.names)
